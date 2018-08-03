bbc93eb6bf269c7e843046fab5f03f9950ec57b3
XANkui <1272200579@qq.com>
2018-08-03 14:23:56 +0800

    添加DOTween插件，并在UIPanel添加动画

--- Assets/Scripts/UIPanel/InfoPanel.cs
+++ Assets/Scripts/UIPanel/InfoPanel.cs
@@ -2,18 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UIFramework_XAN;
+using DG.Tweening;
 
 /// <summary>
 /// Info panel.
 /// </summary>
 public class InfoPanel : BaseUIPanel {
 
+	private Vector3 originPos;		//UIPanel 的原始位置参数
+	private Vector3 targetPos;		//UIPanel 的目标位置参数
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake(){
+		
+		//UIPanel 的原始位置
+		//设置 UIPanel 目标位置
+		//把目标位置设置为 UIPanel 的当前位置
+		originPos = this.transform.localPosition;
+		targetPos = originPos + new Vector3 (0, 600, 0);
+		transform.localPosition = targetPos;
+
+	}
+
 	/// <summary>
 	/// UIPanle 进入时的回调函数
 	/// </summary>
 	public override void OnEnterCallBack ()
 	{
+		//判断 UIPanel 进入前是否移到画布外
+		if(originPos == transform.localPosition){
+			//UIPanel 的原始位置
+			//设置 UIPanel 目标位置
+			//把目标位置设置为 UIPanel 的当前位置
+			originPos = this.transform.localPosition;
+			targetPos = originPos + new Vector3 (0, 600, 0);
+			transform.localPosition = targetPos;
+				
+		}
+
 		base.OnEnterCallBack ();
+
+		//做动画，UIpanel 进入
+		transform.DOLocalMove (originPos, 0.5f);
 
 		#if DebugState
 
@@ -55,8 +87,11 @@ public class InfoPanel : BaseUIPanel {
 	/// </summary>
 	public override void OnExitCallBack ()
 	{
-		base.OnExitCallBack ();
-
+		//执行完动画，再隐藏 UIPanel
+		transform.DOLocalMove (targetPos, 0.3f).OnComplete (()=>{
+			base.OnExitCallBack ();
+		});
+			
 		#if DebugState
 
 		Debug.Log (" InfoPanel 退出");
--- Assets/Scripts/UIPanel/SettingPanel.cs
+++ Assets/Scripts/UIPanel/SettingPanel.cs
@@ -2,18 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UIFramework_XAN;
+using DG.Tweening;
 
 /// <summary>
 /// Setting panel.
 /// </summary>
 public class SettingPanel : BaseUIPanel {
 
+	private Vector3 originPos;		//UIPanel 的原始位置参数
+	private Vector3 targetPos;		//UIPanel 的目标位置参数
+
+	void Awake(){
+		//UIPanel 的原始位置
+		//设置 UIPanel 目标位置
+		//把目标位置设置为 UIPanel 的当前位置
+		originPos = this.transform.localPosition;
+		targetPos = originPos + new Vector3 (0, 600, 0);
+		transform.localPosition = targetPos;
+
+	}
+
 	/// <summary>
 	/// UIPanle 进入时的回调函数
 	/// </summary>
 	public override void OnEnterCallBack ()
 	{
+		//判断 UIPanel 进入前是否移到画布外
+		if(originPos == transform.localPosition){
+			//UIPanel 的原始位置
+			//设置 UIPanel 目标位置
+			//把目标位置设置为 UIPanel 的当前位置
+			originPos = this.transform.localPosition;
+			targetPos = originPos + new Vector3 (0, 600, 0);
+			transform.localPosition = targetPos;
+
+		}
+
+		//执行基类调用
 		base.OnEnterCallBack ();
+
+		//做动画，UIpanel 进入
+		transform.DOLocalMove (originPos, 0.5f);
 
 		#if DebugState
 
@@ -55,7 +84,10 @@ public class SettingPanel : BaseUIPanel {
 	/// </summary>
 	public override void OnExitCallBack ()
 	{
-		base.OnExitCallBack ();
+		//执行完动画，再隐藏 UIPanel
+		transform.DOLocalMove (targetPos, 0.3f).OnComplete (()=>{
+			base.OnExitCallBack ();
+		});
 
 		#if DebugState
 
